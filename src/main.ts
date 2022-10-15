14d7f495751451e294e9a3979ab053fc90c74fa8
nbert71 <nicobert2205@gmail.com>
2022-10-15 20:43:03 +0200

    ajout des CSRF

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableCors();
+  app.use(csurf())
   await app.listen(parseInt(process.env.APP_PORT));
 }
 bootstrap();
