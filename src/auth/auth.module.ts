d680c2947c4a932eb9af447bd6abb5df5c759758
nbert71 <nicobert2205@gmail.com>
2022-10-15 20:27:17 +0200

    ⚡️ jwt works finally !

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt'
 import { jwtConstants } from './constants';
 import { JwtStrategy } from './jwt.strategy';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import { JwtStrategy } from './jwt.strategy';
       signOptions: {expiresIn: '60s'},
     })
   ],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService,LocalStrategy, JwtStrategy],
   exports: [AuthService]
 })
 export class AuthModule {}
